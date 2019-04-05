f48324016ef5cb5e4720d9864a0b384a62098e00
aquaraga <aquaraga@gmail.com>
2019-04-05 16:33:33 +0530

    HTML statement

--- src/main/kotlin/movierental/Customer.kt
+++ src/main/kotlin/movierental/Customer.kt
@@ -18,4 +18,17 @@ class Customer(val name: String) {
         result += "You earned " + _rentals.totalPoints().toString() + " frequent renter points"
         return result
     }
+
+    fun htmlStatement(): String {
+        var result = "<H1>Rentals for <EM>$name</EM></H1><P>\n"
+        for (each in _rentals) {
+            // show figures for this rental
+            result += "${each.movie.title}: ${each.amount()}<BR>\n"
+        }
+        // add footer lines
+        result += "<P>You owe <EM>${_rentals.totalAmount()}</EM><P>\n"
+        result += "On this rental you earned <EM>${_rentals.totalPoints()}</EM> frequent renter points<P>"
+
+        return result //To change body of created functions use File | Settings | File Templates.
+    }
 }
